0dbe17baf0de1c67239dcaa4580956901c7f0871
Nemo <commits@captnemo.in>
2022-12-02 13:51:53 +0530

    [fix] Ignore 5xx responses from Kuvera
    If we get a 5xx on a batch, split that batch into two and continue
    till we find a failing batch with count=1 - mark it as failed.
    
    We run this on batches of 150, so we effeciently try to make 30 calls.
    This increases the overhead somewhat, by adding ~8 calls to find the
    broken identifier which returns a 5xx. But still, we get the whole
    lookup table within 40 API calls, so that's quite good.

--- _scripts/fetch.sh
+++ _scripts/fetch.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
 IFS='|'
-str="'$*'"
-curl --request GET \
-  --silent \
-  --url "https://api.kuvera.in/mf/api/v5/fund_schemes/$str.json" \
-  --header 'Content-Type: application/json' | \
-  jq -r '.[] | [.ISIN,.code]|@csv' >> lookup.csv
+
+fetch(){
+  c="$#"
+  f=$(mktemp -u --suffix=.json)
+  curl --request GET --fail \
+    --silent \
+    --url "https://api.kuvera.in/mf/api/v5/fund_schemes/$*.json" \
+    --header 'Content-Type: application/json' --output $f
+  if [ $? -ne 0 ]; then
+    if [ $c -ne 1 ]; then
+      half=$((c/2))
+      left=("${*:1:$half}")
+      right=("${*:$half+1}")
+      # Invoke an explicit subshell for recursion
+      (fetch $left )
+      (fetch $right )
+    else
+      echo "[FAIL] $*"
+    fi
+  else
+    cat "$f" | jq -r '.[] | [.ISIN,.code]|@csv' >> lookup.csv
+  fi  
+}
+
+fetch $*
