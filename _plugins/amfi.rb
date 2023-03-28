f5bbd5a707868bed4adafea5d385785f50ff42f4
Nemo <commits@captnemo.in>
2023-03-28 18:25:52 +0530

    Use historical-mf-data repo instead for NAV

--- _plugins/amfi.rb
+++ _plugins/amfi.rb
@@ -1,25 +1,37 @@
-require 'csv'
 require 'json'
+require 'sqlite3'
+
+# Open database for reading only
+DB = SQLite3::Database.new('funds.db', :readonly => true)
+
+def get_historical_nav(isin)
+  DB.execute("SELECT date, nav from nav_by_isin WHERE isin='#{isin}' ORDER BY date")
+end
+
+def get_latest_nav(isin)
+  DB.execute("SELECT nav,date from nav_by_isin WHERE isin='#{isin}' ORDER BY date DESC LIMIT 1")[0]
+end
+
+def get_mutual_fund_schemes
+  DB.execute("SELECT isin,scheme_name as name,S1.scheme_code FROM securities S1
+    LEFT JOIN schemes S2 ON S1.scheme_code = S2.scheme_code")
+end
 
 Jekyll::Hooks.register :site, :after_init do |site|
-  data = CSV.read('_data/nav.csv', col_sep: ';', headers: ['code', 'isin', 'reinvestment_isin', 'name', 'nav', 'date'])[1..]
-  data.each do |nav_data|
-    date = Date.parse(nav_data['date']).strftime('%Y-%m-%d')
+  get_mutual_fund_schemes.each do |isin, scheme_name|
+    puts "[+] #{isin}, #{scheme_name}"
+    nav = get_latest_nav(isin)
+
     data = {
-      'ISIN'=> nav_data['isin'],
-      'name'=> nav_data['name'],
-      'nav' => nav_data['nav'].to_f,
-      'date'=> date
+      'ISIN'=> isin,
+      'name'=> scheme_name,
     }
-    File.write("nav/#{data['ISIN']}", data.to_json) if data['ISIN'].length == 12
-    if nav_data['reinvestment_isin'].length == 12
-      data = {
-        'ISIN'=> nav_data['reinvestment_isin'],
-        'name'=> nav_data['name'] + " (Reinvestment)",
-        'nav' => nav_data['nav'],
-        'date'=> date
-      }
-      File.write("nav/#{data['ISIN']}", data.to_json)
+    if nav
+      data['nav'] = nav[0]
+      data['date']= nav[1]
     end
+
+    data['historical_nav'] = get_historical_nav(data['ISIN'])
+    File.write("nav/#{data['ISIN']}", data.to_json) 
   end
 end
